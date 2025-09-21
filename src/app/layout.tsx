d370701273a6afbadb74ff3e23fd35561729d200
Keretcha <i.keretchashvili97@gmail.com>
2025-09-21 16:27:26 +0400

    chore: installed hero ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+/** @format */
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { HeroUIProvider } from "@heroui/system";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang='en'>
+      <HeroUIProvider>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {children}
+        </body>
+      </HeroUIProvider>
     </html>
   );
 }
